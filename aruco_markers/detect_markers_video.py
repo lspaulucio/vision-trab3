054ded3cc7123fb199b447297166ada4f896edb7
Leonardo Santos Paulucio <leonardo@lcad06.lcad.lan>
2019-07-01 11:26:21 -0300

    First commit

---
+++ aruco_markers/detect_markers_video.py
@@ -0,0 +1,28 @@
+import cv2
+import numpy as np
+from cv2 import aruco
+import matplotlib.pyplot as plt
+import sys
+
+
+
+
+file_name = "videos/camera-01.mp4"
+aruco_dict = aruco.Dictionary_get(aruco.DICT_4X4_1)
+parameters =  aruco.DetectorParameters_create()
+vid = cv2.VideoCapture(file_name)
+
+while True:
+    _, img = vid.read()
+    if img is None:
+        print("Empty Frame")
+        break
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
+    frame_markers = aruco.drawDetectedMarkers(img.copy(), corners, ids)
+    cv2.imshow('output', frame_markers)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cv2.destroyAllWindows()
---
+++ trab3.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+"""Trabalho 3 - Entrega até 10/07
+
+Neste trabalho vocês deverão detectar o robô nos vídeos das 4 câmeras do espaço inteligente e obter a reconstrução
+da  sua posição 3D no mundo. Feito isso, vocês deverão gerar um gráfico da posição do robô, mostrando a trajetória
+que ele realizou.
+
+Para detectar o robô será usado um marcador ARUCO acoplado a sua plataforma. Rotinas de detecção desse tipo de marcador
+poderão ser usadas para obter sua posição central, assim como as suas quinas nas imagens. Essas informações, juntamente
+com os dados de calibração das câmeras, poderão ser usadas para localização 3D do robô.
+
+Informações a serem consideradas:
+- O robô está identificado por um marcador do tipo ARUCO - Código ID 0 (zero) - Tamanho 30 x 30 cm
+- Os vídeos estão sincronizados para garantir que a cada quadro vocês estarão processando imagens do robô capturadas no mesmo instante.
+- A calibração das câmeras é fornecida em 4 arquivos no formato JSON (Para saber como ler os dados, basta procurar no Google).
+- Rotinas de detecção dos marcadores Aruco em imagens e vídeo são fornecidas logo abaixo.
+
+ATENÇĂO: Conforme comentado em sala de aula, existem rotinas de detecção de ARUCO que já fornecem sua localização e orientação 3D,
+se a calibração da câmera e o tamanho do padrão forem fornecidas. Essas rotinas poderão ser usadas para fazer comparações com a
+reconstrução 3D fornecida pelo trabalho de vocês, mas não serão aceitas como o trabalho a ser feito. Portanto, lembrem-se que vocês
+deverão desenvolver a rotina de reconstrução, a partir da detecção do ARUCO acoplado ao robô nas imagens 2D capturadas nos vídeos.
+"""
